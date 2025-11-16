feff60d3adf664e2e98a488f9e792266ea5d2e0b
C <cmann1@users.noreply.github.com>
2025-11-16 19:42:21 +0200

    Add docfx

---
+++ .docs/build.bat
@@ -0,0 +1,4 @@
+docfx docfx.json --serve
+REM After the above command, open a new console and run `docfx docfx.json` to update.
+REM Depending on changes it may be necessary to clear out the `_sites` and `api/*.yml` files,
+REM and run `docfx metadata` to regenerate some of the api yml files.
